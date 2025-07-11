f09b0f7831155f806398d493f7ee633794fc14ca
Ben Clarke <benclarke.website@gmail.com>
2025-07-11 21:42:06 +0100

    Replaced `agent.py` and `agent_executor.py` with `LangGraphAgentExecutor`

--- main.py
+++ main.py
@@ -5,16 +5,33 @@ from a2a.server.apps import A2AStarletteApplication
 from a2a.server.request_handlers import DefaultRequestHandler
 from a2a.server.tasks import InMemoryTaskStore
 from a2a.types import AgentCapabilities, AgentCard, AgentSkill
+from a2anet.executors.langgraph import LangGraphAgentExecutor
+from a2anet.types.langgraph import StructuredResponse
+from langchain_tavily import TavilySearch
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.prebuilt import create_react_agent
 
-from agent_executor import TavilyAgentExecutor
-
-skill = AgentSkill(
-    id="search-web",
-    name="Search Web",
-    description="Search the web with the Tavily API and answer questions about the results.",
-    tags=["search", "web", "tavily"],
-    examples=["Who is Leo Messi?"],
+SYSTEM_INSTRUCTION: str = (
+    "You are a helpful assistant that can search the web with the Tavily API and answer questions about the results.\n"
+    "If the `tavily_search` tool returns insufficient results, you should explain that to the user and ask them to try again with a more specific query.\n"
+    "You can use markdown format to format your responses."
 )
+
+RESPONSE_FORMAT_INSTRUCTION: str = (
+    "You are an expert A2A protocol agent.\n"
+    "Your task is to read through all previous messages throughly and determine what the state of the task is.\n"
+    "If the task is complete, extract the task output into an artifact. The task is complete if the `tavily_search` tool has been called and the results are sufficient to answer the user's question."
+)
+
+graph = create_react_agent(
+    model="anthropic:claude-sonnet-4-20250514",
+    tools=[TavilySearch(max_results=2)],
+    checkpointer=MemorySaver(),
+    prompt=SYSTEM_INSTRUCTION,
+    response_format=(RESPONSE_FORMAT_INSTRUCTION, StructuredResponse),
+)
+
+agent_executor: LangGraphAgentExecutor = LangGraphAgentExecutor(graph)
 
 # Get port from environment variable
 port: int = int(os.getenv("PORT"))
@@ -29,11 +46,17 @@ agent_card: AgentCard = AgentCard(
     defaultInputModes=["text", "text/plain"],
     defaultOutputModes=["text", "text/plain"],
     capabilities=AgentCapabilities(),
-    skills=[skill],
+    skills=[AgentSkill(
+        id="search-web",
+        name="Search Web",
+        description="Search the web with the Tavily API and answer questions about the results.",
+        tags=["search", "web", "tavily"],
+        examples=["Who is Leo Messi?"],
+    )],
 )
 
 request_handler: DefaultRequestHandler = DefaultRequestHandler(
-    agent_executor=TavilyAgentExecutor(), task_store=InMemoryTaskStore()
+    agent_executor=agent_executor, task_store=InMemoryTaskStore()
 )
 
 server: A2AStarletteApplication = A2AStarletteApplication(
